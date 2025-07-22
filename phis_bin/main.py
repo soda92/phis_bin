03fa304476d9224435e982c8f0eb17666bef8815
Maple <wjxa20152015@gmail.com>
2025-07-22 09:20:43 +0800

    init code

---
+++ phis_bin/main.py
@@ -0,0 +1,58 @@
+from pathlib import Path
+import argparse
+import shutil
+
+CR = Path(__file__).parent
+
+
+def main():
+    BIN_DIR = CR / 'BIN'
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--init-from',
+        type=str,
+        help='Initialize the binary directory with existing binaries.',
+    )
+
+    parser.add_argument(
+        '--init',
+        help='copy BIN and config files to the current directory',
+        action='store_true',
+    )
+
+    args = parser.parse_args()
+
+    if args.init:
+        if not BIN_DIR.exists() and not BIN_DIR.is_dir():
+            print('please init from existing binaries first.')
+            return
+        else:
+            shutil.copytree(BIN_DIR, Path.cwd() / 'BIN')
+            shutil.copytree(CR / '配置文件', Path.cwd() / '配置文件')
+
+    original_bin: str = args.init_from
+    if original_bin is None:
+        # print help
+        parser.print_help()
+        return
+
+    if not Path(original_bin).is_absolute():
+        original_bin = Path.cwd() / original_bin
+    else:
+        original_bin = Path(original_bin)
+
+    bindir = original_bin / 'BIN'
+    if not bindir.exists() or not bindir.is_dir():
+        print(f'BIN directory {bindir} does not exist.')
+        return
+    else:
+        if not BIN_DIR.exists():
+            shutil.copytree(bindir, BIN_DIR)
+    CONFIG_DIR = original_bin / '配置文件'
+    local_config_dir = CR / '配置文件'
+    if not CONFIG_DIR.exists() or not CONFIG_DIR.is_dir():
+        print(f'CONFIG directory {CONFIG_DIR} does not exist.')
+        return
+    if not local_config_dir.exists():
+        shutil.copytree(CONFIG_DIR, local_config_dir)
